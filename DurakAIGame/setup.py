74be21a0cc4cceb62142c58a79da29a9604e8c83
Gecko! <74975165+SmartGecko44@users.noreply.github.com>
2024-01-05 00:04:42 +0100

    Add sonarcloud check

---
+++ DurakAIGame/setup.py
@@ -0,0 +1,25 @@
+#from setuptools import setup, find_packages
+#
+#setup(
+#    name='DurakAIGame',
+#    version='1.0.0',
+#    author='Gecko!',
+#    author_email='74975165+SmartGecko44@users.noreply.github.com',
+#    description='Play Durak against AI',
+#    packages=find_packages(),
+#    install_requires=[
+#        # List your dependencies here
+#    typing,
+#    time,
+#    zip,
+#    unittest,
+#    warnings
+#
+#    ],
+#    entry_points={
+#        'console_scripts': [
+#            # Define any command-line scripts here
+#        ],
+#    },
+#)
+#
